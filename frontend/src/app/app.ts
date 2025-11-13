aee6c1470e7f6874ebe3cc999f7c7f2c22b06d95
Alires Rosa <alires.cardoso@ufpr.br>
2025-11-12 23:38:38 -0300

    feat: initial project setup with Spring Boot and Angular

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { TesteService } from './services/teste.service';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.html',
+  styleUrls: ['./app.css']
+})
+export class AppComponent implements OnInit {
+  mensagem = '';
+
+  constructor(private testeService: TesteService) {}
+
+  ngOnInit(): void {
+    this.testeService.ping().subscribe({
+      next: msg => this.mensagem = msg,
+      error: err => this.mensagem = 'Erro ao conectar: ' + err.message
+    });
+  }
+}
