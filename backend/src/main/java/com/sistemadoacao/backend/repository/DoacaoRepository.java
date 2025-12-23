5801713c467ce0e547980f70f63c75a6287e51ae
Alires Rosa <alires.cardoso@ufpr.br>
2025-12-23 00:35:34 -0300

    feat: create DoacaoRepository

--- backend/src/main/java/com/sistemadoacao/backend/repository/DoacaoRepository.java
+++ backend/src/main/java/com/sistemadoacao/backend/repository/DoacaoRepository.java
@@ -1,13 +1,10 @@
 package com.sistemadoacao.backend.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.sistemadoacao.backend.model.Usuario;
+import com.sistemadoacao.backend.model.Doacao;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface DoacaoRepository extends JpaRepository<Doacao, Long> {
 
-    boolean existsByEmail(String email);
-    
 }
