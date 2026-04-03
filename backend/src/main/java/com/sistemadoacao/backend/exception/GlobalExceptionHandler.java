b541a547561fbd3e5f7e9e44a09590b5fdea247b
Alires Rosa <alires.cardoso@ufpr.br>
2026-04-02 22:57:21 -0300

    feat: add custom exception classes

---
+++ backend/src/main/java/com/sistemadoacao/backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,51 @@
+package com.sistemadoacao.backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFoundException(NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ocorreu um erro inesperado.");
+    }
+
+    @ExceptionHandler(EquipamentNullException.class)
+    public ResponseEntity<String> handleEquipamentNullException(EquipamentNullException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(StatusNullException.class)
+    public ResponseEntity<String> handleStatusNullException(StatusNullException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(IdNullException.class)
+    public ResponseEntity<String> handleIdNullException(IdNullException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(FileStorageException.class)
+    public ResponseEntity<String> handleFileStorageException(FileStorageException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ErroCadastoException.class)
+    public ResponseEntity<String> handleErroCadastroException(ErroCadastoException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(RequestImageIaException.class)
+    public ResponseEntity<String> handleRequestImageIaException(RequestImageIaException ex) {
+        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(ex.getMessage());
+    }
+
+}
